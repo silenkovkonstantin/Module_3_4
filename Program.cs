bffa5e47ff56dc5e33d810deba0d14c5a87f6110
silenkovkonstantin <silenkov@inbox.ru>
2022-12-31 16:14:52 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass2
+{
+    public static void Main(string[] args)
+    {
+    }
+    enum Semaphore
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300
+    }
+
+}
